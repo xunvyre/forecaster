a2065a796f5cba7a0484d4715dde82c279c1a6ed
Victoria David <victoria@thedavids.us>
2022-07-20 17:03:55 -0400

    uvi added, css updated with necessary styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,6 +23,7 @@ var newSearch = function(event)
 
 var getWeatherInfo = function(city)
 {
+    //call the API and get the needed data
     var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" +city+ "&units=imperial&appid=5027f45d9847017607fd34991ce9cbc7";
     fetch(apiURL).then(function(response)
     {
@@ -30,22 +31,27 @@ var getWeatherInfo = function(city)
         {
             response.json().then(function(data)
             {
-                displayTodaysInfo(data, city);
+                //push data to new functions
+                displayTodaysInfo(data);
+                createButton(data);
             });
         }
         else
         {
+            //return error
             alert("Error: City not found!");
         }
     })
     .catch(function(error)
     {
+        //return error
         alert("Unable to connect to OpenWeather :(");
     });
 };
 
-var displayTodaysInfo = function (data, city)
+var displayTodaysInfo = function (data)
 {
+    //create elements for today's weather
     var bigWeatherBox = document.querySelector("#todays-weather");
     bigWeatherBox.innerHTML = "";
 
@@ -64,11 +70,55 @@ var displayTodaysInfo = function (data, city)
     var bigHum = document.createElement("p");
     bigHum.textContent = "Humidity: " +data.main.humidity+ "%";
 
+    //append created elements
     bigWeatherBox.appendChild(bigTitle);
     bigTitle.appendChild(bigIcon);
     bigWeatherBox.appendChild(bigTemp);
     bigWeatherBox.appendChild(bigWSpeed);
     bigWeatherBox.appendChild(bigHum);
+
+    var fiveDayURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" +data.coord.lat+ "&lon=" +data.coord.lon+ "&units=imperial&exclude=minutely,hourly,alerts&appid=5027f45d9847017607fd34991ce9cbc7";
+    fetch(fiveDayURL).then(function(response)
+    {
+        response.json().then(function(fiveDayData)
+        {
+            //first, use new data to add UV level to bigWeatherBox 
+            var bigUV = document.createElement("p");
+            bigUV.textContent = "UV Index: ";
+            
+            var value = document.createElement("button");
+            value.textContent = fiveDayData.current.uvi;
+            value.setAttribute = ("id", "bUVI");
+            if (fiveDayData.current.uvi <= 3)
+            {
+                value.classList = "btn uv1 col-2";
+            }
+            else if (fiveDayData.current.uvi > 7)
+            {
+                value.classList = "btn uv3 col-2";
+            }
+            else
+            {
+                value.classList = "btn uv2 col-2";
+            }
+
+            bigWeatherBox.appendChild(bigUV);
+            bigUV.appendChild(value);
+        });
+    });
 };
 
+var createButton = function(data)
+{
+    var appendButton = document.querySelector("#prev-searches");
+
+    var newButton = document.createElement("button");
+    newButton.textContent = data.name;
+    newButton.classList = "btn col-12";
+
+    appendButton.appendChild(newButton);
+    //store in localStorage
+}
+
+//get prev searches on page load
 citySearchEl.addEventListener("submit", newSearch);
